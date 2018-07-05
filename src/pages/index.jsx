12631d8912a86de04ed7106f7e1cce327fe33ddb
Roman Vesely <crazko@gmail.com>
2018-07-05 17:15:29 +0200

    Test error

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import { graphql } from 'gatsby';
 
 import { getPosts } from 'utils';
 
-import { Layout } from 'components/Layout';
+// import { Layout } from 'components/Layout';
 import { Posts } from 'components/Posts';
 import { Centered } from 'components/Centered';
 import { Container } from 'components/Container';
@@ -11,27 +11,27 @@ import { Button } from 'components/Button';
 
 export default ({ data: { allMarkdownRemark: { edges: posts } } }) => {
   return (
-    <Layout>
-      <Container>
-        <section>
-          <h1>O Paralelnej Polis</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis
-            dolor et ad accusamus quibusdam nostrum ipsam harum consequuntur
-            architecto, dolorum ipsa tempora aliquid labore consectetur
-            asperiores vel libero maiores distinctio.
-          </p>
-          <Button to="/o-paralelnej-polis">Viac info</Button>
-        </section>
-        <section>
-          <h1>Blog</h1>
-          <Posts posts={getPosts(posts)} />
-          <Centered>
-            <Button to="/blog">Všetky príspevky</Button>
-          </Centered>
-        </section>
-      </Container>
-    </Layout>
+    // <Layout>
+    <Container>
+      <section>
+        <h1>O Paralelnej Polis</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis
+          dolor et ad accusamus quibusdam nostrum ipsam harum consequuntur
+          architecto, dolorum ipsa tempora aliquid labore consectetur asperiores
+          vel libero maiores distinctio.
+        </p>
+        <Button to="/o-paralelnej-polis">Viac info</Button>
+      </section>
+      <section>
+        <h1>Blog</h1>
+        <Posts posts={getPosts(posts)} />
+        <Centered>
+          <Button to="/blog">Všetky príspevky</Button>
+        </Centered>
+      </section>
+    </Container>
+    // </Layout>
   );
 };
 
