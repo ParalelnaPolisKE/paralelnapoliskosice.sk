f73b60134287a3f6059a35d84846d8a90a7e30bc
Roman Vesely <crazko@gmail.com>
2018-07-05 17:15:29 +0200

    Test error

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
 
-import { Layout } from 'components/Layout';
+// import { Layout } from 'components/Layout';
 import { Container } from 'components/Container';
 
 export const Page = ({ children, title }) => (
-  <Layout>
-    <Container>
-      <Helmet title={title} />
-      <h1>{title}</h1>
-      {children}
-    </Container>
-  </Layout>
+  // <Layout>
+  <Container>
+    <Helmet title={title} />
+    <h1>{title}</h1>
+    {children}
+  </Container>
+  // </Layout>
 );
 
 Page.propTypes = {
