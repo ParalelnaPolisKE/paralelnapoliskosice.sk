9092650d982c65de46e34e3476e1be726aee5d60
Roman Vesely <crazko@gmail.com>
2018-07-07 16:07:11 +0200

    Fix post cover

--- src/utils.js
+++ src/utils.js
@@ -5,5 +5,5 @@ export const getPosts = posts =>
     url: post.node.fields.url,
     excerpt: post.node.excerpt,
     tags: post.node.frontmatter.tags || [],
-    imageSizes: post.node.cover ? post.node.cover.sizes : null,
+    imageSizes: post.node.cover ? post.node.cover.fluid : null,
   }));
