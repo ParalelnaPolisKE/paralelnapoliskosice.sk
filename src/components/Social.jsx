854c4f43b6c4a116fbc7ce5541cb54117fecdacc
Roman Vesely <crazko@gmail.com>
2018-07-10 16:23:06 +0200

    Update styles

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -7,7 +7,11 @@ export const Social = ({ data }) => (
   <div className={css.social}>
     {Object.keys(data).map(title => (
       <a href={data[title]} className={css.site} key={title}>
-        <img className={css.socialIcon} src={`/assets/social/${title}.png`} />
+        <img
+          className={css.socialIcon}
+          src={`/assets/social/${title}.png`}
+          alt={title}
+        />
       </a>
     ))}
   </div>
