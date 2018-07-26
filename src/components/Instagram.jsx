d7fab880e8bd05075bbeac278ce2ef01278a02c6
Roman Vesely <crazko@gmail.com>
2018-07-26 16:35:14 +0200

    Add images into site

---
+++ src/components/Instagram.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import css from './Instagram.module.css';
+
+import { Container } from 'components/Container';
+import InstagramImages from 'components/InstagramImages';
+
+export const Instagram = () => (
+  <div className={css.instagram}>
+    <Container className={css.instaContainer}>
+      <InstagramImages>
+        {images =>
+          images.map(image => <div className={css.instaImage}>{image}</div>)
+        }
+      </InstagramImages>
+    </Container>
+  </div>
+);
