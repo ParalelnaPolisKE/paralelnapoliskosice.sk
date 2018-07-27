efe1a9cb8591d8f0e4c6dc41c9fc49eaf1a20d12
Roman Vesely <crazko@gmail.com>
2018-07-27 23:09:35 +0200

    Use Fetch API for insta photos

--- utils/instagram/scrape.js
+++ utils/instagram/scrape.js
@@ -3,13 +3,16 @@
 require('dotenv').config();
 
 const fs = require(`fs`);
-const request = require(`request`);
+const fetch = require('node-fetch');
 const mkdirp = require(`mkdirp`);
 const ProgressBar = require(`progress`);
 const download = require(`./download-file`);
 
-const access_token = process.env.INSTAGRAM_ACCESS_TOKEN;
-const url = `https://api.instagram.com/v1/users/self/media/recent/?count=5&access_token=${access_token}`;
+const asstetsPath = './static/assets/instagram';
+const accessToken = process.env.INSTAGRAM_ACCESS_TOKEN;
+const url = `https://api.instagram.com/v1/users/self/media/recent/?count=5&access_token=${accessToken}`;
+
+const log = message => console.error(`Error: ${message}`);
 
 // Convert timestamp to ISO 8601.
 const toISO8601 = timestamp => new Date(timestamp * 1000).toJSON();
@@ -19,36 +22,22 @@ const bar = new ProgressBar(
   `Downloading instagram posts [:bar] :current/:total :elapsed secs :percent`,
   {
     total: 0,
-    width: 30,
+    width: 10,
   }
 );
 
 // Create the images directory
-mkdirp.sync(`./static/assets/instagram`);
+mkdirp.sync(asstetsPath);
 
-let posts = [];
-
-// Write json
-const saveJSON = _ =>
-  fs.writeFileSync(
-    `./static/assets/instagram/instagram.json`,
-    JSON.stringify(posts, ``, 2)
-  );
-
-const getPosts = () => {
-  request(url, { encoding: `utf8` }, (err, res, body) => {
-    if (err) {
-      console.log(`error: ${err}`);
-    }
-
-    body = JSON.parse(body);
-
+fetch(url)
+  .then(response => response.json())
+  .then(body => {
     if (body.meta.code === 400) {
-      console.log(`error: ${body.meta.error_message}`);
+      log(body.meta.error_message);
       process.exit(1);
     }
 
-    body.data
+    const images = body.data
       .filter(image => image['type'] === 'image')
       .map(image => ({
         id: image.id,
@@ -56,19 +45,22 @@ const getPosts = () => {
         link: image.link,
         media: image.images.standard_resolution.url,
         image: `./${image.id}.jpg`, // relative path to images from .json
-      }))
-      .forEach(image => {
-        // Download image locally and update progress bar
-        bar.total++;
-        download(image.media, `./static/assets/instagram/${image.id}.jpg`, _ =>
-          bar.tick()
-        );
+      }));
 
-        posts.push(image);
-      });
+    // Download image locally and update progress bar
+    images.forEach(image => {
+      bar.total++;
+      download(image.media, `${asstetsPath}/${image.id}.jpg`, response =>
+        bar.tick()
+      );
+    });
 
-    saveJSON();
+    fs.writeFileSync(
+      `${asstetsPath}/instagram.json`,
+      JSON.stringify(images, '', 2)
+    );
+  })
+  .catch(error => {
+    log(error.message);
+    process.exit(1);
   });
-};
-
-getPosts();
