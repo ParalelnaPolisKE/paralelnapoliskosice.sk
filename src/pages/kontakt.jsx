74e670d8209315337e70ffff138dd7d8c1066ff4
Roman Vesely <crazko@gmail.com>
2018-08-03 17:48:14 +0200

    Add new contact info

--- src/pages/kontakt.jsx
+++ src/pages/kontakt.jsx
@@ -18,6 +18,17 @@ export default ({
     </div>
     <div>
       <h2>Sociálne siete</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit repellat
+        cumque, ratione quidem quod alias, iste, voluptatem explicabo qui
+        laudantium impedit quis? Eius odio suscipit explicabo dicta, dolore
+        facere maxime.
+      </p>
+      <ul>
+        <li>srgsg</li>
+        <li />
+        <li />
+      </ul>
       <Social data={social} />
     </div>
   </Page>
