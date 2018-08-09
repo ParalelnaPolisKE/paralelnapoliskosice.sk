cb7c916b00400a161ab93cf2c6d9f8743eaee396
Roman Vesely <crazko@gmail.com>
2018-08-09 14:14:00 +0200

    Add dummy text

--- src/pages/o-paralelnej-polis.jsx
+++ src/pages/o-paralelnej-polis.jsx
@@ -2,4 +2,13 @@ import React from 'react';
 
 import { Page } from 'components/Page';
 
-export default () => <Page title="O Paralelnej Polis" />;
+export default () => (
+  <Page title="O Paralelnej Polis">
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur
+      autem, inventore excepturi eos doloremque reprehenderit provident
+      architecto blanditiis sint nemo quasi at explicabo praesentium. Tempora
+      vero aperiam totam velit consequuntur.
+    </p>
+  </Page>
+);
