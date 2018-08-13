da39d30e72d341bd66360db9de8d8a9dfc88c609
Ljuk67 <lukascech1@gmail.com>
2018-08-13 10:36:43 +0200

    index & o pp page text update

--- src/pages/o-paralelnej-polis.jsx
+++ src/pages/o-paralelnej-polis.jsx
@@ -4,11 +4,63 @@ import { Page } from 'components/Page';
 
 export default () => (
   <Page title="O Paralelnej Polis">
+    <blockquote>
+      Paralelná Polis Košice: ostrov slobody a nezávislosti.
+    </blockquote>
+    <h2>Kto sme?</h2>
     <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur
-      autem, inventore excepturi eos doloremque reprehenderit provident
-      architecto blanditiis sint nemo quasi at explicabo praesentium. Tempora
-      vero aperiam totam velit consequuntur.
+      Sme kolektív ľudí, ktorí chcú žiť v slobodnejšom a otvorenejšom svete.
+      Hľadáme spôsoby a technológie (Bitcoin, blockchain, reputačné systémy a
+      decentralizované technológie), ktoré nám otvárajú možnosti, zjednodušujú
+      bežné procesy a odstraňujú bariéry. V Paralelnej Polis Košice chceme
+      vytvoriť prostredie zamerané na vzdelávanie, objavovanie a tvorenie
+      lepších systémov pre naše okolie - teba, nás, firmy, jednotlivcov -
+      všetkých, ktorí majú záujem fungovať slobodnejšie a nezávislejšie.
+      Zameriavame sa na experimentovanie a aplikáciu kryptomien a spojených
+      technológií do bežného života.
+    </p>
+    <p>
+      Dnešné technológie a ich exponenciálny rozvoj priniesli veľké výhody ako
+      pre jednotlivcov, tak pre firmy, ktoré sa týkajú hlavne väčšej slobody a
+      otvorenosti, rýchlejších transakcií, nižších poplatkov, decentralizácie.
+      No tieto sú pre väčšinu ľudí ešte neznáme. Naším cieľom je ich priblížiť,
+      vysvetliť a pomôcť uplatniť v praxi. Máme ambíciu byť experimentátormi a
+      inovátormi v týchto perspektívnych oblastiach.
+    </p>
+    <p>
+      Inšpirovali sme sa tímom{' '}
+      <a href="https://paralelnapolis.sk/" target="_blank">
+        Paralelnej Polis v Bratislave
+      </a>{' '}
+      a{' '}
+      <a href="https://www.paralelnipolis.cz/" target="_blank">
+        Paralelní Polis v Prahe
+      </a>{' '}
+      a týmto chceme túto myšlienku naším spôsobom priblížiť aj Košiciam a
+      okoliu.
+    </p>
+    <h2>Filozofia / Vízia</h2>
+    <p>
+      Našou víziou je svet, v ktorom majú ľudia možnosť odlúčiť sa od štátu a
+      žiť v slobodnej spoločnosti. Vytvorenie priestoru, kde bude mať každý
+      možnosť sa informovať, vzdelávať, rozvíjať a posúvať seba či svoj biznis
+      vopred prostredníctvom nových technológií, kryptomien, slobodnejšej
+      spoločnosti a podmienok pre prácu, podnikanie či kariéru.
+    </p>
+    <p>
+    <b>Ako našu víziu realizujeme?</b>
+    </p>
+    <p>Našou hlavnou činnosťou je vzdelávanie a pomoc pri adopcii kryptomien a spojených
+       technológií, ktoré umožnia slobodnejšie     fungovanie jednotlivca aj spoločnosti ako takej.
+       Našu víziu realizujeme prostredníctvom:
+       <ul>
+         <li><b>Prednášok</b> - u nás, u vás</li>
+         <li><b>Technickej podpory</b> - zájdi k nám a radi vysvetlíme ako môžu kryptomeny / blockchain pomôcť tvojmu projektu či
+    biznisu</li>
+    <li><b>Spolupráce</b> - umožňujeme tvorivcom a podnikavcom realizovať či
+    vyskúšať svoje nápady, pomáhame s ich aplikáciou alebo s adopciou
+    existujúcich technológií</li>
+    </ul>
     </p>
   </Page>
 );
