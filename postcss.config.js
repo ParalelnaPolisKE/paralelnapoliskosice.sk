03384a568c4f74cd0835bf8601b98aebb9b5157d
Roman Vesely <crazko@gmail.com>
2018-10-08 22:12:47 +0200

    Purge unused css classes

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,5 @@
 const tailwindcss = require('tailwindcss');
-const purgecss = require('@fullhuman/postcss-purgecss');
 
 module.exports = {
-  plugins: [
-    tailwindcss('./tailwind.js'),
-    // purgecss({
-    // content: ['./src/**/*.html'],
-    // }),
-  ],
+  plugins: [tailwindcss('./tailwind.js')],
 };
---
+++ utils/purgecss-tailwind-extractor.js
@@ -0,0 +1,7 @@
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
+
+module.exports = TailwindExtractor;
