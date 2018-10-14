3d7077a0b68a7284abad6dd80b46e1f6ed17e399
Roman Vesely <crazko@gmail.com>
2018-10-14 22:45:17 +0200

    Remove newsletter pages from indexing

--- src/pages/newsletter-ok.jsx
+++ src/pages/newsletter-ok.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 import { Page } from 'components/Page';
 
 export default () => (
   <Page title="Prihlásenie bolo úspešné" showNewsletter={false}>
+    <Helmet>
+      <meta name="robots" content="noindex" />
+    </Helmet>
+
     <p>Odber newslettera je úspešne potvrdený.</p>
     <p>Ďakujeme</p>
   </Page>
--- src/pages/newsletter-potvrdenie.jsx
+++ src/pages/newsletter-potvrdenie.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 import { Page } from 'components/Page';
 
 export default () => (
   <Page title="Už sme skoro tam..." showNewsletter={false}>
+    <Helmet>
+      <meta name="robots" content="noindex" />
+    </Helmet>
+
     <p>Potrebujeme potvrdiť tvoju emailovú adresu.</p>
     <p>
       Na dokončenie prihlásenia, prosím, klikni na link v emaily, ktorý sme ti
