5f9dcbc6c35fcdce93d31da48e6e68b0388d4b81
Roman Vesely <crazko@gmail.com>
2018-11-16 08:40:53 +0100

    Remove past event

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,7 +17,7 @@ export default ({
     <Layout showNewsletter>
       <Container>
         <div className="lg:flex -mx-4">
-          <section className="lg:w-1/2 px-4">
+          <section className="px-4">
             <h1>O Paralelnej Polis</h1>
             <p>
               Sme kolektív ľudí, ktorí chcú žiť v slobodnejšom a otvorenejšom
@@ -39,35 +39,6 @@ export default ({
               bežného života.
             </p>
             <Button to="/o-paralelnej-polis">Viac informácií</Button>
-          </section>
-          <section className="lg:w-1/2 px-4">
-            <h1>Udalosti</h1>
-            <h2>
-              <a href="https://www.facebook.com/events/959009994284372/">
-                Predstavenie + otvorenie Bitcoin ATM
-              </a>
-            </h2>
-            <p>
-              <strong>14. november - 17:00</strong>,{' '}
-              <a href="http://www.kinousmev.sk/">Kino Úsmev</a>
-            </p>
-            <p>
-              Oficiáln(ejšie) predstavenie Paralelnej Polis Košice a spustenie
-              kryptomatu.
-            </p>
-            <ul>
-              <li>Čo je Paralelná Polis Košice?</li>
-              <li>Na čom pracujeme, čo chceme dosiahnuť?</li>
-              <li>Našich cieľoch a výzvach</li>
-              <li>
-                Pokecaj si s členmi tímu - pridaj svoj nápad, daj nám spätnú
-                väzbu
-              </li>
-              <li>Vyskúšaj si kryptomat - automat na nákup kryptomien</li>
-            </ul>
-            <Button to="https://www.facebook.com/events/959009994284372/">
-              Viac informácií v udalosti
-            </Button>
           </section>
         </div>
         <section>
