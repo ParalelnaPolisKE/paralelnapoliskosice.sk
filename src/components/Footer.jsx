e42f0bd1bb6b3e1f145fd36999b90768756e6238
Roman Vesely <crazko@gmail.com>
2018-12-22 10:29:52 +0100

    Update footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import classnames from 'classnames';
 import css from './Footer.module.css';
 
 import { Container } from './Container';
@@ -8,27 +10,55 @@ import { Social } from './Social';
 import { Newsletter } from 'components/Newsletter';
 import InstagramImages from 'components/InstagramImages';
 
-export const Footer = () => (
+export const Footer = ({ showNewsletter }) => (
   <footer role="contentinfo">
-    <div className={`${css.top} px-4 pb-8 md:pb-0`}>
-      <Container className="md:flex">
-        <div className="md:w-2/3 mb-4 md:mb-0 py-4 sm:py-8">
-          <h2 className="text-grey-darker m-0">Zostaňme v kontakte</h2>
-          <p className="mb-2">Prihlás sa na odber newslettera:</p>
-          <Newsletter className="flex flex-col sm:flex-row mb-2" />
-          <p className="text-xs text-grey-dark m-0">
-            Z času na čas ťa budeme informovať o aktuálnom dianí z prostredia
-            Paralelnej Polis.
-            <br />
-            Žiaden spam - len relevantné informácie.
-          </p>
-        </div>
-        <div className="bg-grey-dark md:w-1/3 px-2 py-1">
-          <div className="-ml-1 -mr-1 md:-mr-3 flex flex-wrap">
+    <div
+      className={classnames(
+        css.top,
+        { [css.topStripe]: showNewsletter },
+        'p-4 sm:py-8',
+        { 'lg:py-0': showNewsletter }
+      )}
+    >
+      <Container className={classnames({ 'lg:flex': showNewsletter })}>
+        {/* Newsletter */}
+        {showNewsletter && (
+          <div className="lg:w-2/3 mb-8 lg:my-8">
+            <h2 className="text-grey-darker m-0">Zostaňme v kontakte</h2>
+            <p className="mb-2">Prihlás sa na odber newslettera:</p>
+            <Newsletter className="flex flex-col sm:flex-row mb-2" />
+            <p className="text-xs text-grey-dark m-0">
+              Z času na čas ťa budeme informovať o aktuálnom dianí z prostredia
+              Paralelnej Polis.
+              <br />
+              Žiaden spam - len relevantné informácie.
+            </p>
+          </div>
+        )}
+        {/* Instagram */}
+        <div
+          className={classnames(
+            'bg-grey-dark p-1',
+            showNewsletter && 'lg:w-1/3 lg:flex'
+          )}
+        >
+          <div
+            className={classnames(
+              'flex flex-wrap',
+              showNewsletter && 'lg:content-center lg:w-full'
+            )}
+          >
             <InstagramImages>
               {images =>
                 images.map((image, i) => (
-                  <div key={i} className={`${css.instaImage} p-1 w-1/3`}>
+                  <div
+                    key={i}
+                    className={classnames(
+                      css.instaImage,
+                      'p-1 w-1/3 sm:w-1/6',
+                      { 'lg:w-1/3': showNewsletter }
+                    )}
+                  >
                     {image}
                   </div>
                 ))
@@ -41,7 +71,8 @@ export const Footer = () => (
 
     <div className="px-4">
       <Container className="py-4 sm:py-8 flex items-center justify-center sm:justify-start">
-        <strong className="sm:text-lg mr-4">Nájdete nás</strong> <Social />
+        <strong className="hidden sm:block sm:text-lg mr-4">Nájdete nás</strong>{' '}
+        <Social />
       </Container>
     </div>
 
@@ -59,6 +90,7 @@ export const Footer = () => (
           <p className="font-light">
             designed by{' '}
             <a href="https://www.matusdesign.com">matusdesign.com</a>
+            <Link to="/newsletter">odkaz</Link>
           </p>
         </div>
 
