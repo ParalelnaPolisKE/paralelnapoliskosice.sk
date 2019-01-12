0a32634fcd2ddb478ce37d9a8dce088a5f5dbbdf
Roman Veselý <crazko@gmail.com>
2019-01-12 16:50:24 +0100

    Mobile nav (#77)
    * Add mobile nav foundation
    
    * Update mobile nav design
    
    * Update mobile nav design
    
    * Fix nav pointer events
    
    * Update mobile header, closes #21
    
    * typo
    
    * Make mobile nav button more visible

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,20 +1,88 @@
 import React from 'react';
+import classnames from 'classnames';
 
 import { NavigationButton } from './NavigationButton';
 
-export const Navigation = () => (
-  <nav
-    role="navigation"
-    className="flex flex-col items-end md:items-center md:flex-row"
-  >
-    <NavigationButton to="/" exact>
-      Domov
-    </NavigationButton>
-    <NavigationButton to="/o-paralelnej-polis">
-      O Paralelnej Polis
-    </NavigationButton>
-    <NavigationButton to="/rychlokurz-bezpecnosti">Rýchlokurz</NavigationButton>
-    <NavigationButton to="/blog">Blog</NavigationButton>
-    <NavigationButton to="/kontakt">Kontakt</NavigationButton>
-  </nav>
-);
+export class Navigation extends React.Component {
+  state = {
+    isToggled: true,
+    isSmallScreen: false,
+  };
+
+  componentDidMount() {
+    this.mediaQueryList = window.matchMedia('(min-width: 576px)');
+    this.mediaQueryList.addListener(this.handleScreenWidth);
+
+    this.handleScreenWidth(this.mediaQueryList);
+  }
+
+  componentWillUnmount() {
+    this.mediaQueryList.removeListener(this.handleScreenWidth);
+  }
+
+  handleScreenWidth = e => {
+    if (e.matches) {
+      // Big screen
+      this.setState(() => ({
+        isToggled: true,
+        isSmallScreen: false,
+      }));
+    } else {
+      // Small screen
+      this.setState(() => ({
+        isToggled: false,
+        isSmallScreen: true,
+      }));
+    }
+  };
+
+  handleToggle = () =>
+    this.setState(({ isToggled }) => ({
+      isToggled: !isToggled,
+    }));
+
+  render() {
+    return (
+      <nav
+        role="navigation"
+        className={classnames(
+          'fixed sm:static z-50 pin-b pin-r sm:pin-none p-3 pl-12 pt-12 sm:p-0  sm:bg-transparent flex flex-col items-end md:items-center md:flex-row',
+          {
+            'bg-grey-light shadow':
+              this.state.isSmallScreen && this.state.isToggled,
+          },
+          {
+            'pointer-events-none':
+              this.state.isSmallScreen && !this.state.isToggled,
+          }
+        )}
+      >
+        {this.state.isToggled && [
+          <NavigationButton to="/" exact>
+            Domov
+          </NavigationButton>,
+          <NavigationButton to="/o-paralelnej-polis">
+            O Paralelnej Polis
+          </NavigationButton>,
+          <NavigationButton to="/rychlokurz-bezpecnosti">
+            Rýchlokurz
+          </NavigationButton>,
+          <NavigationButton to="/blog">Blog</NavigationButton>,
+          <NavigationButton to="/kontakt">Kontakt</NavigationButton>,
+        ]}
+        {this.state.isSmallScreen && (
+          <div
+            onClick={this.handleToggle}
+            className="bg-grey hover:bg-grey-dark text-xl p-3 mt-8 rounded-full shadow-md cursor-pointer pointer-events-auto"
+          >
+            {this.state.isToggled ? (
+              <span className="icon-cancel" />
+            ) : (
+              <span className="icon-menu" />
+            )}
+          </div>
+        )}
+      </nav>
+    );
+  }
+}
