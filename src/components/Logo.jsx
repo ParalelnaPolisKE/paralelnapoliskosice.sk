fcfc702e27efdb2b4a30a9c033a5b326e6a2af81
Roman Vesely <crazko@gmail.com>
2019-02-21 10:15:56 +0100

    Update logo and make it responsive, closes #33

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import logo from '../images/logo.svg';
+import logoLong from '../images/logo-long.svg';
 import css from './Logo.module.css';
 
 export const Logo = () => (
-  <Link className={`${css.logo} inline-block w-12 sm:w-24`} to="/">
-    <img src={logo} className="max-w-full" alt="logo" />
+  <Link className={`${css.logo} inline-block`} to="/">
+    <picture>
+      <source
+        media="(max-width: 575px)"
+        srcSet={logoLong}
+        type="image/svg+xml"
+      />
+
+      <img src={logo} alt="logo" className="w-64" />
+    </picture>
   </Link>
 );
