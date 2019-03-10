f906aec738c4226afde2215b591efb993e621a4f
Roman Vesely <crazko@gmail.com>
2019-03-10 19:52:15 +0100

    Add navigation link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,8 @@ import { Navigation } from './Navigation';
 export const Header = () => (
   <header className="bg-grey-lightest p-4">
     <Container>
-      <div className="flex">
-        <div className="flex-1 flex items-center justify-center sm:justify-start">
+      <div className="flex md:flex-col lg:flex-row">
+        <div className="flex-1 flex items-center justify-center sm:justify-start md:mb-4 lg:mb-0">
           <Logo />
         </div>
         <Navigation />
