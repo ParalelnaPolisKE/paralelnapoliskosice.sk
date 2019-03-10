276187d8198bc4b61aaa9497a4db00cf94998c3b
Roman Vesely <crazko@gmail.com>
2019-03-10 16:00:06 +0100

    Add mdx for pages

--- src/pages/404.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { Page } from 'components/Page';
-
-export default () => (
-  <Page title="Not found">
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Page>
-);
--- src/pages/podmienky-pouzivania.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { Page } from 'components/Page';
-
-export default () => (
-  <Page title="Podmienky používania" url="/podmienky-pouzivania">
-    bude doplnené
-  </Page>
-);
