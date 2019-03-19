912d7a087eb4605893b272aff541e77fa72455e0
Roman Veselý <crazko@gmail.com>
2019-03-19 21:56:41 +0100

    Zapoj sa (#86)
    * Remove undefined css class
    
    * Add navigation link
    
    * Add "Zapoj sa" section
    
    * Add thanks gif
    
    * Use frontmatter for pages meta data
    
    * Change wording
    
    * Make newsletter MDX
    
    * Move "zapoj sa" index
    
    * Reorder content
    
    * Add contact
    
    * Add contact form
    
    * Add confirmation page
    
    * Update form texts
    
    * Update "sent" page text
    
    * typo
    
    * Remove "len tak" section
    
    Will be reworded later

---
+++ src/components/FormJoin.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export const FormJoin = ({ children, category }) => (
+  <form
+    name="join"
+    method="POST"
+    action="/zapoj-sa/ok"
+    data-netlify="true"
+    className="p-4 mb-8 bg-grey-lightest leading-none md:flex"
+  >
+    <textarea
+      name="message"
+      rows="10"
+      className="text-sm border p-4 mb-2 md:m-0 w-full md:flex-1"
+      defaultValue={children}
+    />
+    <div className="flex flex-col flex-1 md:ml-2">
+      <input
+        name="name"
+        placeholder="tvoje meno"
+        type="text"
+        className="text-sm border p-4 mb-2"
+        required
+      />
+      <input
+        autoCapitalize="off"
+        autoCorrect="off"
+        name="email"
+        placeholder="email"
+        type="email"
+        className="text-sm border p-4 mb-2"
+        required
+      />
+      <input name="subject" type="hidden" value={category} />
+      <input
+        name="submit"
+        type="submit"
+        value="Chcem sa zapojiť!"
+        className="text-xs font-semibold uppercase p-4 bg-grey-dark text-white hover:bg-grey-darker cursor-pointer"
+      />
+    </div>
+  </form>
+);
