e4d1ce0fca565928f4da05287ba0b4c2ec840e7c
Roman Vesely <crazko@gmail.com>
2024-10-10 15:13:01 +0200

    changes

--- src/data.js
+++ src/data.js
@@ -12,30 +12,6 @@ export const data = {
     x: "https://twitter.com/parallelpoliske",
     rss: "/blog/rss.xml",
   },
-  crypto: {
-    BTC: "1KGB5uxAZrFYrHKsydLvfhwT4VULE1tunA",
-    LTC: "Lha1KBxm5wgtTEJdk1tyBhWgAuxJ2zV9zX",
-    XMR: "49XCoar5nDSiz3QfSa25jyJ5KJSc95pkpS8YXWVoB3TY8LDVjEjhUoZRRQL5sidfLB6cUWnxH2Tv5VbGKzxxieJqQPPHuea",
-    ETH: "0x610825C5DFcbc72E284E5a5F381f4fd728263706",
-    ETC: "0x1934945354BDe5b34F0c68E6AA78492050856D38",
-  },
-  nodes: {
-    BTC: {
-      mainnet: {
-        url: "btc.ppke.sk",
-      },
-    },
-    LTC: {
-      mainnet: {
-        url: "ltc.ppke.sk",
-      },
-    },
-    XMR: {
-      mainnet: {
-        url: "xmr.ppke.sk:18081",
-      },
-    },
-  },
   mailchimpUrl:
     "https://paralelnapoliskosice.us19.list-manage.com/subscribe/post?u=8affbd08463d07e25a8bbcca4&id=b02c302d92",
 };
